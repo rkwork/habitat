849c2d83f9c90a4800287661f6100f7c0b8e9c69
Dave Parfitt <dparfitt@chef.io>
2016-07-27 17:46:04 -0400

    wip Signed-off-by: Dave Parfitt <dparfitt@chef.io>

---
+++ test/hab_inspec/controls/clean_env.rb
@@ -0,0 +1,36 @@
+hab_env_vars = %w(HAB_AUTH_TOKEN
+                  HAB_CACHE_KEY_PATH
+                  HAB_DEPOT_URL
+                  HAB_ORG
+                  HAB_ORIGIN
+                  HAB_ORIGIN_KEYS
+                  HAB_RING
+                  HAB_RING_KEY
+                  HAB_STUDIOS_HOME
+                  HAB_STUDIO_ROOT
+                  HAB_USER)
+
+
+hab_ports = [9631, 9632, 9636]
+
+hab_ports.each do |p|
+    describe port(p) do
+        puts "Ensuring port #{p} isn't open"
+        it { should_not be_listening }
+    end
+end
+
+hab_env_vars.each do |e|
+    describe os_env(e) do
+        puts "Ensuring #{e} isn't set"
+        it { should eq nil }
+    end
+end
+
+describe user('hab') do
+    it { should exist }
+end
+
+
+
+
